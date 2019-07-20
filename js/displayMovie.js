efe69f9e805f6685dd3636460dc9ffbb0586db4f
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-07-20 18:23:48 +0530

    Refactored file to use innerHTML instead of createElement

--- js/displayMovie.js
+++ js/displayMovie.js
@@ -1,35 +1,31 @@
 import { extractYear } from "./utils";
+
 const imageURL = "https://image.tmdb.org/t/p/w500/";
 
 function displayMovie(movie) {
   const app = document.getElementById("app");
 
+  // extracting required items from the object
   const { title, release_date, poster_path } = movie;
 
   // creating components
   const movieCard = document.createElement("div");
-  const movieImage = document.createElement("img");
-  const movieInfo = document.createElement("div");
-  const movieTitle = document.createElement("span");
-  const movieYear = document.createElement("span");
-
-  // setting attributes
   movieCard.setAttribute("class", "card");
-  movieImage.setAttribute("class", "card__image");
-  movieInfo.setAttribute("class", "card__info");
-  movieTitle.setAttribute("class", "card__title");
-  movieYear.setAttribute("class", "card__year");
 
-  movieTitle.innerText = title;
-  movieYear.innerText = extractYear(release_date);
-  movieImage.src = imageURL + poster_path;
+  movieCard.innerHTML = `
+    <div class="card__image__container">
+      <img class="card__image" src=${imageURL + poster_path} />
+      <div class="card__image__overlay">
+        <button class="btn btn--primary">Add</button>
+      </div>
+    </div>
+    <div class="card__info">
+      <span class="card__title">${title}</span>
+      <span class="card__year">${extractYear(release_date)}</span>
+    </div>
+  `;
 
-  // appending to html
-  movieInfo.appendChild(movieTitle);
-  movieInfo.appendChild(movieYear);
-  movieCard.appendChild(movieImage);
-  movieCard.appendChild(movieInfo);
-
+  // appending card to html
   app.appendChild(movieCard);
 }
 
